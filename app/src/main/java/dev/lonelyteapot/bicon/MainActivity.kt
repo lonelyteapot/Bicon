5ea8729eaf3416b79c4c3048da62801416ac7003
Dmitry Semenov <lonelyteapot@gmail.com>
2021-08-25 00:17:05 +0300

    Add keyboard press handler

--- app/src/main/java/dev/lonelyteapot/bicon/MainActivity.kt
+++ app/src/main/java/dev/lonelyteapot/bicon/MainActivity.kt
@@ -2,10 +2,34 @@ package dev.lonelyteapot.bicon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import dev.lonelyteapot.bicon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onKeyboardKeyClick(key: View) {
+        val orig = binding.editNewSpending.text.toString()
+        binding.editNewSpending.text = when (key) {
+            binding.keyboard.keyEnter -> {
+                "0"
+            }
+            binding.keyboard.keyBackspace -> {
+                if (orig.length == 1) "0"
+                else orig.dropLast(1)
+            }
+            else -> {
+                val text = (key as Button).text
+                if (orig == "0") text
+                else orig + text
+            }
+        }
     }
 }
