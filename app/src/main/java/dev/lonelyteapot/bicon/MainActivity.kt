73ad444c6fe493dd14f514e01d769d6cf4e5fb3b
Dmitry Semenov <lonelyteapot@gmail.com>
2021-09-01 13:45:06 +0300

    Replace main activity

--- app/src/main/java/dev/lonelyteapot/bicon/MainActivity.kt
+++ app/src/main/java/dev/lonelyteapot/bicon/MainActivity.kt
@@ -2,8 +2,6 @@ package dev.lonelyteapot.bicon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import dev.lonelyteapot.bicon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.editNewSpending.addTextChangedListener(LeadingZeroTextWatcher())
-        binding.editTotalLeft.addTextChangedListener(LeadingZeroTextWatcher())
-        binding.editDateUntil.addTextChangedListener(DateTextWatcher())
     }
 }
